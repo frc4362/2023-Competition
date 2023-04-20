18b0b739634eee82dc35a748e239e8ed9b19d602
Ethan Malzone <ejmalzone@gmail.com>
2023-04-20 15:44:44 -0400

    After Practice Matches
    Adjust center balance auton to score cone like other autons
    Adjust cable side auton pathing

--- src/main/java/com/gemsrobotics/robot/autos/BalanceAuto.java
+++ src/main/java/com/gemsrobotics/robot/autos/BalanceAuto.java
@@ -3,6 +3,7 @@ package com.gemsrobotics.robot.autos;
 import com.gemsrobotics.robot.commands.PlaceCommand;
 import com.gemsrobotics.robot.commands.DriveOntoPlatform;
 import com.gemsrobotics.robot.subsystems.Claw;
+import com.gemsrobotics.robot.subsystems.Superstructure;
 import com.gemsrobotics.robot.subsystems.SuperstructurePose;
 import com.gemsrobotics.robot.subsystems.Swerve;
 import edu.wpi.first.math.geometry.Translation2d;
@@ -13,8 +14,9 @@ import edu.wpi.first.wpilibj2.command.WaitCommand;
 public class BalanceAuto extends SequentialCommandGroup {
 	public BalanceAuto(final Swerve swerve) {
 		addCommands(
+				new InstantCommand(() -> Superstructure.getInstance().setWantedState(Superstructure.WantedState.STARTING)),
 				Claw.getInstance().requestGrab(), 
-				new PlaceCommand(SuperstructurePose.HIGH_PLACE),
+				new PlaceCommand(SuperstructurePose.AUTON_PLACE),
 				new DriveOntoPlatform(swerve, new Translation2d(.35, 0.0), .15),
 				new InstantCommand(() -> swerve.setDrivePercent(new Translation2d(.32, 0.0), 0.0, true, false)),
 				new WaitCommand(1.2),
@@ -22,6 +24,6 @@ public class BalanceAuto extends SequentialCommandGroup {
 				new InstantCommand(() -> swerve.setDrivePercent(new Translation2d(0, 0), 0.25, true, false)),
 				new WaitCommand(0.25),
 				swerve.getStopCommand()
-		);
+		);																																																																																																																																								
 	}
 }
